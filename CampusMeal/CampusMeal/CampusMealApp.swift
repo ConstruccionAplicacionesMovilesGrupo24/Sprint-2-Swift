0647bcfff42acd1434f359da93b55b010c55a5c9
dvargasl2 <d.vargasl2@uniandes.edu.co>
2026-09-17 22:47:01 -0500

    Add CampusMeal Xcode project, reorganize iOS source folder

---
+++ CampusMeal/CampusMeal/CampusMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusMealApp.swift
+//  CampusMeal
+//
+//  Created by Daniel Vargas on 17/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CampusMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
